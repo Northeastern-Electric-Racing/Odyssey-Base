31f3e56a23da20ca0feef898b014beece20d6d66
Peyton-McKee <mckee.p@northeastern.edu>
2023-09-17 17:34:25 -0400

    Init

---
+++ src/types/message.types.ts
@@ -0,0 +1,27 @@
+import { Topic } from "./topic";
+
+/**
+ * The format of a message sent from the server
+ */
+export type ServerMessage = {
+  unix_time: number;
+  node: string;
+  data: ServerData[];
+};
+
+type ServerData = {
+  name: string;
+  value: number;
+  units: string;
+};
+
+/**
+ * The format of the message sent to subscribe or unsubscribe from a topic
+ */
+export type SubscriptionMessage = {
+  argument: SubscriptionArgument;
+  topics: Topic[];
+};
+
+type SubscriptionArgument = 'subscribe' | 'unsubscribe';
+
